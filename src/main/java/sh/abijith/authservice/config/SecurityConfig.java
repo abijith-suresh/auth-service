4024a1a3346e09408674e3a00ec7079709c5d18a
Abijith S <abijithsuresh47@gmail.com>
2025-04-11 00:22:15 +0530

    Adding security config for admin-server

--- src/main/java/sh/abijith/authservice/config/SecurityConfig.java
+++ src/main/java/sh/abijith/authservice/config/SecurityConfig.java
@@ -1,18 +1,29 @@
 package sh.abijith.authservice.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Value("${AUTH_ACTUATOR_USERNAME:admin}")
+    private String actuatorUsername;
+
+    @Value("${AUTH_ACTUATOR_PASSWORD}")
+    private String actuatorPassword;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -25,12 +36,26 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/actuator/**").permitAll()
                         .anyRequest().authenticated()
                 )
+                .httpBasic(Customizer.withDefaults())
                 .sessionManagement(sess -> sess
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 );
 
         return http.build();
     }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService(){
+        UserDetails admin = User.builder()
+                .username(actuatorUsername)
+                .password(actuatorPassword)
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin);
+    }
+
 }
